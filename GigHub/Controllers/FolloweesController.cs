e116859b55cd45e2c86a7f330c4ce26292530741
nicolas.delforno <nicolas.delforno@intelex.com>
2021-01-15 15:51:52 -0500

    Add view and controller for followees

---
+++ GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Following
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
