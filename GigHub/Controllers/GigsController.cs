c6ca10f822a06ae487e8eb8b1a9d26bc06ed898d
nicolas.delforno <nicolas.delforno@intelex.com>
2021-01-17 18:19:04 -0500

    Add my upcomings gigs view

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -2,7 +2,6 @@
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -51,7 +50,7 @@ namespace GigHub.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
 
         [Authorize]
@@ -74,6 +73,21 @@ namespace GigHub.Controllers
 
             return View("Gigs", viewModel);
         }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
+              
+        }
+
+        
 
     }
 }
