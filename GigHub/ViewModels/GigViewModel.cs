5b75bdcc28dd9c9bc337259d0559238548f287da
Nicolas Delforno <nicolas.delforno@intelex.com>
2021-04-12 16:40:22 -0400

    Gig detail page creation

---
+++ GigHub/ViewModels/GigViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GigHub.Models;
+
+
+namespace GigHub.ViewModels
+{
+    public class GigViewModel
+    {
+        public Gig Gig { get; set; }
+        public bool IsFollowing { get; set; }
+        public bool IsGoing { get; set; }
+
+    }
+}
